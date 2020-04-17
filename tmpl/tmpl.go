6310d569ce740a5bf3c4521a366eafff18f2794d
Bruno Luiz Silva <contact@brunoluiz.net>
2020-04-17 10:58:34 +0100

    chore: mermaid version fixed on 7.1.2

--- tmpl/tmpl.go
+++ tmpl/tmpl.go
@@ -14,10 +14,9 @@ const IndexHTML = `<!doctype html>
 	</div>
 	<div class="error"></div>
 	<script src="http://localhost:35729/livereload.js"></script>
-	<script src="https://unpkg.com/mermaid/dist/mermaid.min.js"></script>
+	<script src="https://unpkg.com/mermaid@7.1.2/dist/mermaid.min.js"></script>
 	<script>
 		mermaid.initialize({
-			theme: 'forest',
 			startOnLoad:true
 		});
 
